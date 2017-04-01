46a17cbf595193899e9836a9c23762bdf864b27b
BrunoDaguis <bruno.daguis@shiftinc.com.br>
2017-03-31 21:26:51 -0300

    init

---
+++ app.js
@@ -0,0 +1,175 @@
+var express = require('express');
+var mongoose = require('mongoose');
+
+var userModel = require('./model/user.js');
+var postModel = require('./model/post.js');
+var commentModel = require('./model/comment.js');
+var imagesModel = require('./model/image.js');
+
+var likeModel = require('./model/like.js');
+
+var bodyParser= require('body-parser');
+
+var app = express();
+
+
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var options = {
+		db: { native_parser: true },
+		server: { poolSize: 5 },
+		replset: { rs_name: 'myReplicaSetName' },
+		user: 'sa',
+		pass: 'admin123'
+	};
+
+mongoose.connect('mongodb://ds141950.mlab.com:41950/blog', options);
+
+
+app.listen(3000, () => {
+	console.log('listening on 3000')
+});
+
+/* USER */
+
+app.get('/user', function (req, res) {
+	userModel.get(function(results){
+		res.json(results);
+	});
+});
+
+app.get('/user/:id', function (req, res) {
+	userModel.getById(req.params.id, function(result){
+		res.json(result);
+	});
+});
+
+app.post('/user', (req, res) => {
+	userModel.save(req.body, function(result){
+		res.json({_id: result._id});
+	});
+});
+
+app.put('/user', (req, res) => {
+   userModel.save(req.body, function(result){
+		res.json({ success: true });
+	});
+});
+
+app.delete('/user/:id', (req, res) => {
+	userModel.remove(req.params.id, function(result){
+		res.json({ success: true });
+	});
+});
+
+/* post */
+
+app.get('/post', function (req, res) {
+	postModel.get(function(results){
+		res.json(results);
+	});
+});
+
+app.get('/post/:id', function (req, res) {
+	postModel.getById(req.params.id, function(result){
+		res.json(result);
+	});
+});
+
+app.post('/post', (req, res) => {
+	postModel.save(req.body, function(result){
+		res.json({_id: result._id});
+	});
+});
+
+app.put('/post', (req, res) => {
+   postModel.save(req.body, function(result){
+		res.json({ success: true });
+	});
+});
+
+app.delete('/post/:id', (req, res) => {
+	postModel.remove(req.params.id, function(result){
+		res.json({ success: true });
+	});
+});
+
+/* comments (post) */
+
+app.get('/comment', function (req, res) {
+	commentModel.get(function(results){
+		res.json(results);
+	});
+});
+
+app.get('/comment/:id', function (req, res) {
+	commentModel.getById(req.params.id, function(result){
+		res.json(result);
+	});
+});
+
+app.post('/comment', (req, res) => {
+	commentModel.save(req.body, function(comment){
+		postModel.addComment(req.body.post, comment._id, function(result){
+			res.json({_id: comment._id});
+		});		
+	});
+});
+
+app.put('/comment', (req, res) => {
+   commentModel.save(req.body, function(result){
+		res.json({ success: true });
+	});
+});
+
+app.delete('/comment/:id', (req, res) => {
+	//Busco comment pelo ID
+	commentModel.getById(req.params.id, function(comment){
+		//Removo comment da tabela de post
+		postModel.removecomment(comment.post, comment._id, function(){
+			//Removo comment na tabela de comment
+			commentModel.remove(comment._id, function(){
+				res.json({ success: true });
+			});			
+
+		});	
+
+	});
+});
+
+/* IMAGES */
+app.post('/post/image', (req, res) => {
+	imagesModel.save(req.body, function(image){
+		postModel.addImage(req.body.post, image._id, function(){
+			res.json({_id: image._id});
+		});	
+	});
+});
+
+app.delete('/post/image/:id', (req, res) => {
+	imagesModel.remove(req.params.id, function(image){
+		imagesModel.remove(req.params.id, function(result){
+			postModel.addImage(image.post, image._id, function(){
+				res.json({ success: true });
+			});	
+		});	
+	});	
+});
+
+/* LIKES */
+app.post('/post/like', (req, res) => {
+	likeModel.save(req.body, function(like){
+		postModel.addLike(req.body.post, function(){
+			res.json({_id: like._id});
+		});	
+	});
+});
+
+app.delete('/post/like/:id', (req, res) => {
+	likeModel.remove(req.params.id, function(result){
+		postModel.removeLike(req.body.post, function(){
+			res.json({ success: true });
+		});		
+	});
+});
---
+++ config.js
@@ -0,0 +1,9 @@
+{
+	database: {
+		db: { native_parser: true },
+		server: { poolSize: 5 },
+		replset: { rs_name: 'myReplicaSetName' },
+		user: 'sa',
+		pass: 'admin123'
+	}
+}
